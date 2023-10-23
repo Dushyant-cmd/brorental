4070be31495c654b879c5a267c1306937f5a3a4a
dushyant <dushyant.977360@gmail.com>
2023-10-23 18:52:15 +0530

    rent item list done.

--- app/src/main/java/com/brorental/brorental/activities/RentDetailsActivity.java
+++ app/src/main/java/com/brorental/brorental/activities/RentDetailsActivity.java
@@ -1,16 +1,20 @@
 package com.brorental.brorental.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.brorental.brorental.R;
+import com.brorental.brorental.databinding.ActivityRentDetailsBinding;
 
 public class RentDetailsActivity extends AppCompatActivity {
-
+    private ActivityRentDetailsBinding binding;
+    private String TAG = "RentDetailsActivity.java";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rent_details);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_rent_details);
+
     }
 }
