a894bdbd380aa77d9c8e766e5344eb6beac6f5cc
dushyant <dushyant.977360@gmail.com>
2023-11-14 22:24:15 +0530

    set listeners in ride activity

--- app/src/main/java/com/brorental/brorental/activities/RideActivity.java
+++ app/src/main/java/com/brorental/brorental/activities/RideActivity.java
@@ -1,21 +1,35 @@
 package com.brorental.brorental.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.brorental.brorental.R;
+import com.brorental.brorental.databinding.ActivityRideBinding;
 import com.brorental.brorental.utilities.AppClass;
 import com.brorental.brorental.utilities.Utility;
 
 public class RideActivity extends AppCompatActivity {
     private AppClass appClass;
+    private ActivityRideBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ride);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_ride);
         appClass = (AppClass) getApplication();
         //REGISTER BROADCAST RECEIVER FOR INTERNET
         Utility.registerConnectivityBR(RideActivity.this, appClass);
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
